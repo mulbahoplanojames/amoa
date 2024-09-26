1e6e370ab77d3a3935f370cec4973240c12c8e5c
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-09-26 23:29:02 +0200

    update the project structure by 99% completed the Navbar, hero section and the services section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,11 +35,11 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 bg-blue-400">
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
             src={heroImage}
-            alt="Hero image"
+            alt="techSpace Hero image"
           />
         </div>
       </section>
