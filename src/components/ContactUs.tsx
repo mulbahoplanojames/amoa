15da1dc56460f904277556f96c3f9705de82cbd7
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-09-27 14:23:46 +0200

    update the project structure by 99% completed the services section, footer section and the testimonial section

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -13,25 +13,25 @@ import {
 const ContactUs = () => {
   return (
     <>
-      <section className="py-12 bg-white">
+      <section className="py-12 bg-white dark:bg-transparent">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
               Contact Us
             </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-slate-200">
               Get in Touch
             </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 mx-auto">
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 mx-auto dark:text-slate-400">
               We'd love to hear from you. Send us a message and we'll respond as
               soon as possible.
             </p>
           </div>
 
           <div className="mt-12 grid grid-cols-1 gap-8 lg:grid-cols-2">
-            <Card>
+            <Card className="w-full h-fit dark:bg-white">
               <CardHeader>
-                <CardTitle className="text-2xl pb-2">
+                <CardTitle className="text-2xl pb-2 text-slate-900">
                   Send us a message
                 </CardTitle>
                 <CardDescription className="md:text-lg text-base pb-2">
@@ -109,9 +109,9 @@ const ContactUs = () => {
               </CardContent>
             </Card>
 
-            <Card className="w-full h-fit">
+            <Card className="w-full h-fit dark:bg-white">
               <CardHeader>
-                <CardTitle className="text-2xl pb-2">
+                <CardTitle className="text-2xl pb-2 text-slate-900">
                   Contact Information
                 </CardTitle>
                 <CardDescription className="md:text-lg text-base">
