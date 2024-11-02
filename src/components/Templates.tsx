a7a1162ccc17d0ac1068652ee52e63c637bdd239
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-11-02 11:46:18 +0200

    Updating some components by adding some templates and changing the logo

--- src/components/Templates.tsx
+++ src/components/Templates.tsx
@@ -20,28 +20,28 @@ const Templates = () => {
 
   return (
     <section className="py-12 bg-white dark:bg-transparent" id="/templates">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base font-semibold tracking-wide text-indigo-600 uppercase">
             Templates
           </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+          <p className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl dark:text-white">
             Our Latest Templates
           </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 dark:text-gray-300 mx-auto">
+          <p className="max-w-2xl mx-auto mt-4 text-xl text-gray-500 dark:text-gray-300">
             Explore some of our recent templates and see how we've helped
             businesses achieve their digital goals.
           </p>
         </div>
 
-        <div className="md:mt-10 mt-8 text-center">
+        <div className="mt-8 text-center md:mt-10">
           {/* Category Buttons */}
           {categories.map((category) => (
             <Button
               key={category}
               onClick={() => category && setSelectedCategory(category)}
               variant="outline"
-              className="md:mx-2 mx-4 my-2"
+              className="mx-4 my-2 md:mx-2"
             >
               {category && category.charAt(0).toUpperCase() + category.slice(1)}
             </Button>
@@ -53,17 +53,17 @@ const Templates = () => {
             {filteredProjects.map((project) => (
               <div
                 key={project.title}
-                className="flex flex-col rounded-lg shadow-lg overflow-hidden"
+                className="flex flex-col overflow-hidden rounded-lg shadow-lg"
               >
                 <div className="flex-shrink-0">
                   <img
-                    className="h-72 md:h-80 w-full object-cover"
+                    className="object-cover w-full h-72 md:h-80"
                     src={project.image}
                     alt={project.title}
                     width={400}
                   />
                 </div>
-                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex flex-col justify-between flex-1 p-6 bg-white">
                   <div className="flex-1">
                     <p className="text-xl font-semibold text-gray-900">
                       {project.title}
@@ -87,7 +87,7 @@ const Templates = () => {
         </div>
 
         <div className="mt-10 text-center">
-          <Button size="lg">View All Projects</Button>
+          <Button size="lg">View All Templates</Button>
         </div>
       </div>
     </section>
